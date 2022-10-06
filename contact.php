69591aea244dbf1b5afc0f097bd2ad31b4f4af68
Jake Napay <109712790+Jake-Napay@users.noreply.github.com>
2022-10-06 15:07:18 +0800

    Change name extension and employee_image folder created.

---
+++ contact.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+
+  <title>Architectural & Engineering Outsourcing</title>
+  <link rel="stylesheet" href="./css/navbar.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <link rel="stylesheet" href="./css/contact.css">
+  <link rel="stylesheet" href="./css/footer.css">
+
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <nav class="nav">
+    <div class="container-nav">
+      <a href="index.html" class="text-decoration-none d-flex justify-content-center">
+        <img class="nav-logo" src="https://architectoutsourcing.com/wp-content/uploads/2019/05/LOGO-4-2.png" alt="logo">
+      </a>
+      <ul class="menu">
+        <li><a href="index.html" class="">Home</a></li>
+        <li><a href="about.html" class="">About Us</a></li>
+        <li><a href="service.html" class="">Services</a></li>
+        <li><a href="contact.html" class="active">Contact Us</a></li>
+        <li><a href="blog.html" class="">Blog</a></li>
+      </ul>
+
+      <button class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  </nav>
+  <nav class="mobile-nav">
+    <ul>
+      <li><a href="index.html" class="">Home</a></li>
+      <li><a href="about.html" class="">About Us</a></li>
+      <li><a href="service.html" class="">Services</a></li>
+      <li><a href="contact.html" class="active">Contact Us</a></li>
+      <li><a href="blog.html" class="">Blog</a></li>
+    </ul>
+  </nav>
+
+  <div class="wrapper">
+
+    <section id="container-header">
+      <div class="container">
+        <header>
+          <div class="wrapper-bg">
+            <h1 class="header-title">Contact Us</h1>
+            <p class="header-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>Doloribus sunt,
+              sapiente
+              modi dolores ipsum <br>minus odio animi officiis possimus voluptas.</p>
+          </div>
+        </header>
+      </div>
+    </section>
+
+    <section id="first-content">
+      <div class="container">
+        <div class="overflow">
+          <h1 class="content-title">You've got questions,<br>we've got answers.</h1>
+        </div>
+        <div class="row d-flex align-items-center">
+          <div class="col-sm-12 col-md-6 col-lg-6">
+            <form action="">
+              <h2 class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id fugit
+                veritatis, facilis doloremque illo.</h2>
+              <label id="label-name" class="content-label" for="name">Name</label>
+              <input id="name" class="content-input" type="text" placeholder="Name">
+              <label id="label-email" class="content-label" for="email">Email</label>
+              <input id="email" class="content-input" type="email" placeholder="Email Address">
+              <label id="label-message" class="content-label" for="message">Message</label>
+              <textarea name="message" id="message" cols="30" rows="5" placeholder="Message"></textarea>
+              <button id="submit-message" type="submit">Submit</button>
+            </form>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-6">
+            <div class="mapouter">
+              <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas"
+                  src="https://maps.google.com/maps?q=Precision%20888%20Builders,%20Ermin%20Garcia%20Avenue,%20Quezon%20City,%20Metro%20Manila,%20Philippines&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                  frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                <style>
+                  .mapouter {
+                    position: relative;
+                    text-align: right;
+                  }
+                </style><a href="https://www.embedgooglemap.net"></a>
+                <style>
+                  .gmap_canvas {
+                    overflow: hidden;
+                    background: none !important;
+                  }
+                </style>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="second-content">
+      <div class="container">
+        <div class="row">
+          <hr class="mb-5">
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            <h2 class="content-title"><i class="fas fa-map-pin"></i>Title</h2>
+            <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ratione id
+              vel asperiores praesentium quos, consectetur odio? Animi, culpa error.</p>
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            <h2 class="content-title"><i class="fas fa-phone-alt"></i>Title</h2>
+            <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ratione id
+              vel asperiores praesentium quos, consectetur odio? Animi, culpa error.</p>
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            <h2 class="content-title"><i class="fas fa-envelope"></i>Title</h2>
+            <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ratione id
+              vel asperiores praesentium quos, consectetur odio? Animi, culpa error.</p>
+          </div>
+        </div>
+        <hr>
+        <p class="content-subtitle mb-5">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur ipsam tenetur facilis hic est maiores
+          nesciunt dolores totam sapiente mollitia magnam velit officiis necessitatibus assumenda delectus minus,
+          nostrum amet nulla.
+        </p>
+      </div>
+    </section>
+
+
+    <footer>
+      <div class="container">
+        <div class="row g-0 overflow-hidden">
+          <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center">
+            <ul>
+              <!-- <li><img class="img-fluid footer-logo"
+                                    src="https://architectoutsourcing.com/wp-content/uploads/2019/05/LOGO-4-2.png"
+                                    alt="footer logo"> -->
+              <li id="tmpLogo">Logo</li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4 footer-links ">
+            <ul>
+              <h2 class="footer-title">Links</h2>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <ul>
+              <h2 class="footer-title">Contact</h2>
+              <li>
+                <i class="fas fa-phone"></i>09-123-456-789
+              </li>
+              <li>
+                <i class="fas fa-envelope"></i>emailaddress@email.com
+              </li>
+              <li>
+                <i class="fas fa-map-pin"></i></i>City, Country
+              </li>
+            </ul>
+          </div>
+          <hr class="footer-hr">
+          <div class="col-sm-12 col-md-12 col-lg-12 footer-contact mb-5">
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fas fa-envelope"></i>
+            <i class="fas fa-globe"></i>
+            <h2 id="footer-copyright">Copyright &#169 Engineer & Architect Outsourcing</h2>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <!-- bootstrap javascript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- script of aos -->
+  <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+  <!-- to initiate the animation -->
+  <script>
+    AOS.init();
+  </script>
+  <!-- cdn for font -->
+  <script src="https://use.fontawesome.com/984417ca4d.js"></script>
+  <script src="js/navbar.js"></script>
+</body>
+
+</html>
---
+++ service.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+
+  <title>Architectural & Engineering Outsourcing</title>
+  <link rel="stylesheet" href="./css/navbar.css">
+  <link rel="stylesheet" href="./css/styles.css">
+  <link rel="stylesheet" href="./css/service.css">
+  <link rel="stylesheet" href="./css/footer.css">
+
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <nav class="nav">
+    <div class="container-nav">
+      <a href="index.html" class="text-decoration-none d-flex justify-content-center">
+        <img class="nav-logo" src="https://architectoutsourcing.com/wp-content/uploads/2019/05/LOGO-4-2.png" alt="logo">
+      </a>
+      <ul class="menu">
+        <li><a href="index.html" class="">Home</a></li>
+        <li><a href="about.html" class="">About Us</a></li>
+        <li><a href="service.html" class="active">Services</a></li>
+        <li><a href="contact.html" class="">Contact Us</a></li>
+        <li><a href="blog.html" class="">Blog</a></li>
+      </ul>
+
+      <button class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  </nav>
+  <nav class="mobile-nav">
+    <ul>
+      <li><a href="index.html" class="">Home</a></li>
+      <li><a href="about.html" class="">About Us</a></li>
+      <li><a href="service.html" class="active">Services</a></li>
+      <li><a href="contact.html" class="">Contact Us</a></li>
+      <li><a href="blog.html" class="">Blog</a></li>
+    </ul>
+  </nav>
+
+  <div class="wrapper">
+
+    <section id="container-header">
+      <div class="container">
+        <header>
+          <div class="wrapper-bg">
+            <div class="row d-flex align-items-center">
+              <div class="col-sm-12 col-md-6 col-lg-6 order-2 order-md-1 order-lg-1"><img class="img-fluid"
+                  src="images/default-image.png" alt="">
+              </div>
+              <div class=" col-sm-12 col-md-6 col-lg-6 order-1 order-md-2 order-lg-2">
+                <h1 class="header-title">Services</h1>
+                <p class="header-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum omnis fugiat
+                  aliquam possimus modi doloremque doloribus</p>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </section>
+
+    <section id="first-content">
+      <div class="container">
+        <div class="row g-0">
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="wrapper">
+              <img src="images/default-image.png" alt="Photo" class="img-fluid">
+              <h2 class="content-title">Title</h2>
+              <p class="content-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nesciunt minima
+                quia beatae tempore? Perspiciatis.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <footer>
+      <div class="container">
+        <div class="row g-0 overflow-hidden">
+          <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center">
+            <ul>
+              <!-- <li><img class="img-fluid footer-logo"
+                                    src="https://architectoutsourcing.com/wp-content/uploads/2019/05/LOGO-4-2.png"
+                                    alt="footer logo"> -->
+              <li id="tmpLogo">Logo</li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4 footer-links ">
+            <ul>
+              <h2 class="footer-title">Links</h2>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <ul>
+              <h2 class="footer-title">Contact</h2>
+              <li>
+                <i class="fas fa-phone"></i>09-123-456-789
+              </li>
+              <li>
+                <i class="fas fa-envelope"></i>emailaddress@email.com
+              </li>
+              <li>
+                <i class="fas fa-map-pin"></i></i>City, Country
+              </li>
+            </ul>
+          </div>
+          <hr class="footer-hr">
+          <div class="col-sm-12 col-md-12 col-lg-12 footer-contact mb-5">
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fas fa-envelope"></i>
+            <i class="fas fa-globe"></i>
+            <h2 id="footer-copyright">Copyright &#169 Engineer & Architect Outsourcing</h2>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <!-- bootstrap javascript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"
+    integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- script of aos -->
+  <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+  <!-- to initiate the animation -->
+  <script>
+    AOS.init();
+  </script>
+  <!-- cdn for font -->
+  <script src="https://use.fontawesome.com/984417ca4d.js"></script>
+  <script src="js/navbar.js"></script>
+</body>
+
+</html>
